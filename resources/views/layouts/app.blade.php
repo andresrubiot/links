39e85d1c56ad30aaccfccada690bbbd097633949
Andres Rubio <andresrubiot5@gmail.com>
2022-01-11 18:52:17 -0500

    Links on dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,16 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('index') }}">
+                                        Home
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('links.index') }}">
+                                        Links
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('links.create') }}">
+                                        Add Link
+                                    </a>
+                                    <div class="dropdown-divider"></div>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">

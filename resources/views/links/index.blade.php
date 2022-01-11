94a4b52b48d297cfce4a80d160dd451f2710c8b6
Andres Rubio <andresrubiot5@gmail.com>
2022-01-11 18:46:59 -0500

    Links list design

--- resources/views/links/index.blade.php
+++ resources/views/links/index.blade.php
@@ -1,5 +1,18 @@
-<div class="links">
-    @foreach ($links as $link)
-        <a href="{{ $link->url }}">{{ $link->title }}</a>
-    @endforeach
- </div>
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="card-deck">
+            @foreach ($links as $link)
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <a href="{{ $link->url }}">{{ $link->title }}</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
